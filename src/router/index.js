dbcbce3026a7cfb4edcb84aa3d5163acad818d38
“Marcos” <marcos.wiendl@gmail.com>
2025-06-16 12:27:13 -0300

    chore:

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MockList from '../views/MockList.vue'
+import LandingPage from '../views/LandingPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: LandingPage
+    },
     {
       path: '/list/mock',
       name: 'mock-list',
